b02fe6fda4b4d5324f03947c3ce373907e34e72e
tar-alt <hhan93007@gmail.com>
2026-05-04 11:56:40 +0630

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ def send_welcome(message):
 def handle_message(message):
     url = message.text
     if "tiktok.com" in url:
-        msg = bot.reply_to(message, "⏳ ဗီဒီယို ရှာနေပါတယ်...")
+        msg = bot.reply_to(message, "⏳ Video Watermark ဖျောက်နေသည်...")
         try:
             api_url = f"https://www.tikwm.com/api/?url={url}"
             response = requests.get(api_url).json()

1ce95f0c37c83c3eeaa51b0f8ea7b86b3e73dc81
tar-alt <hhan93007@gmail.com>
2026-05-04 12:46:19 +0630

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,13 +8,13 @@ bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "⚡ GitHub Server ပေါ်မှာ Bot အလုပ်လုပ်နေပါပြီ။ Link ပို့ပေးပါ။")
+    bot.reply_to(message, "⚡ Bot စတင်အလုပ်လုပ်နေပါပြီ။ သင် Download လုပ်ချင်တဲ့ TikTok Video Link ပို့ပေးပါ။")
 
 @bot.message_handler(func=lambda message: True)
 def handle_message(message):
     url = message.text
     if "tiktok.com" in url:
-        msg = bot.reply_to(message, "⏳ Video Watermark ဖျောက်နေသည်...")
+        msg = bot.reply_to(TikTok Video Watermark ဖျောက်နေသည်...")
         try:
             api_url = f"https://www.tikwm.com/api/?url={url}"
             response = requests.get(api_url).json()
@@ -25,7 +25,7 @@ def handle_message(message):
                               )
                 bot.delete_message(message.chat.id, msg.message_id)
             else:
-                bot.edit_message_text("❌ Link မှားနေပါသည်။", message.chat.id, msg.message_id)
+                bot.edit_message_text("❌ Link မှားနေပါသည်။ https နဲ့စတဲ့နေရာကစပြီးပို့ပေးပါ ၎င်းရှေ့တွင်တခြားစာလုံးများမပါရ", message.chat.id, msg.message_id)
         except:
             bot.edit_message_text("⚠️ Error တက်သွားပါသည်။", message.chat.id, msg.message_id)
 
